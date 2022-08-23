b594dd70e0f32ac60ad9d27e6b9e55e59819d59e
Salah-boutayeb <boutayebsalaheddin@gmail.com>
2022-08-24 00:41:50 +0100

    v1.0.0

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,51 +1,72 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FormControl, Select, InputLabel, MenuItem } from "@mui/material";
 const Navbar = () => {
   // Toogle Menu
-  const [MobileMenu, setMobileMenu] = useState(false)
+  const [MobileMenu, setMobileMenu] = useState(false);
   return (
     <>
-      <header className='header'>
-        <div className='container d_flex'>
-          <div className='catgrories d_flex'>
-            <span class='fa-solid fa-border-all'></span>
-            <h4>
-              Categories <i className='fa fa-chevron-down'></i>
-            </h4>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="catgrories d_flex">
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={45}
+                label="Age"
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
           </div>
 
-          <div className='navlink'>
-            <ul className={MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
+          <div className="navlink">
+            <ul
+              className={
+                MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
+              }
+              onClick={() => setMobileMenu(false)}
+            >
               {/*<ul className='link f_flex uppercase {MobileMenu ? "nav-links-MobileMenu" : "nav-links"} onClick={() => setMobileMenu(false)}'>*/}
               <li>
-                <Link to='/'>home</Link>
+                <Link to="/">home</Link>
               </li>
               <li>
-                <Link to='/pages'>pages</Link>
+                <Link to="/pages">pages</Link>
               </li>
               <li>
-                <Link to='/user'>user account</Link>
+                <Link to="/user">user account</Link>
               </li>
               <li>
-                <Link to='/vendor'>vendor account</Link>
+                <Link to="/vendor">vendor account</Link>
               </li>
               <li>
-                <Link to='/track'>track my order</Link>
+                <Link to="/track">track my order</Link>
               </li>
               <li>
-                <Link to='/contact'>contact</Link>
+                <Link to="/contact">contact</Link>
               </li>
             </ul>
 
-            <button className='toggle' onClick={() => setMobileMenu(!MobileMenu)}>
-              {MobileMenu ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!MobileMenu)}
+            >
+              {MobileMenu ? (
+                <i className="fas fa-times close home-btn"></i>
+              ) : (
+                <i className="fas fa-bars open"></i>
+              )}
             </button>
           </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
