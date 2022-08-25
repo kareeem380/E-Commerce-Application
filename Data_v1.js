9b3d28ef29142eccb0bfbe10c7711b4451402176
mohamedlaharraf <laharraf.m.fst@uhp.ac.ma>
2022-08-25 16:18:00 +0100

    update

--- Data_v1.js
+++ Data_v1.js
@@ -11,7 +11,6 @@ const Data = {
                 Diamètre: 17,
                 Indice_de_charge: 99,
                 Indice_de_vitesse: 80,
-
             },
             typeVehicule: " ",
             categoryId: 0,
@@ -21,9 +20,33 @@ const Data = {
             updatedAt: "02/09/2022",
             quantite: 10,
 
-            cover: "./images/1.png",
+            cover: "./assets/piece1.jpg",
             name: "Pneus Linglong T010 125/80 R17 99M",
             prix: 2000,
+        },
+        {
+            id: 2,
+            vin: "bla bla bla",
+            reference: "O890274R09Y90",
+            description: {
+                Largeur: 145,
+                Hauteur: 70,
+                Type: "R",
+                Diamètre: 12,
+                Indice_de_charge: 69,
+                Indice_de_vitesse: 40,
+            },
+            typeVehicule: " ",
+            categoryId: 0,
+            fournisseurId: 0,
+            panierId: 0,
+            createdAt: "01/09/2022",
+            updatedAt: "02/09/2022",
+            quantite: 20,
+
+            cover: "./assets/piece2.png",
+            name: "APlus A609 145/70 R12 69T Pneus été",
+            prix: 3100,
         },
 
     ],
