4264be74a4a677f47fbcd68c136ce9b255aad398
mohamedlaharraf <laharraf.m.fst@uhp.ac.ma>
2022-08-25 16:12:15 +0100

    update

---
+++ Data_v1.js
@@ -0,0 +1,31 @@
+const Data = {
+    pieces: [
+        {
+            id: 1,
+            vin: "bla bla bla",
+            reference: "839274R09Y90",
+            description: {
+                Largeur: 125,
+                Hauteur: 80,
+                Type: "R",
+                Diamètre: 17,
+                Indice_de_charge: 99,
+                Indice_de_vitesse: 80,
+
+            },
+            typeVehicule: " ",
+            categoryId: 0,
+            fournisseurId: 0,
+            panierId: 0,
+            createdAt: "01/09/2022",
+            updatedAt: "02/09/2022",
+            quantite: 10,
+
+            cover: "./images/1.png",
+            name: "Pneus Linglong T010 125/80 R17 99M",
+            prix: 2000,
+        },
+
+    ],
+}
+export default Data
