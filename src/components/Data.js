39ac6609eafcbd1b64b6fc1755b0ac5d44cdc9f4
mohamedlaharraf <laharraf.m.fst@uhp.ac.ma>
2022-08-26 16:49:56 +0100

    up

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,5 @@
 const Data = {
- /* productItems: [
+  productItems2: [
     {
       id: 1,
       vin: "bla bla bla",
@@ -123,63 +123,61 @@ const Data = {
       price: 50,
     },
   ],
+  productItems: [
+    {
+      id: 1,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Shoes",
+      price: 100,
+      description:
+        "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
+    },
+    {
+      id: 2,
+      discount: 40,
+      cover: "./images/flash/flash-2.png",
+      name: "Watch",
+      price: 20,
+      description:
+        "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
+    },
+    {
+      id: 3,
+      discount: 40,
+      cover: "./images/flash/flash-3.png",
+      name: "Smart Mobile Black",
+      price: 200,
+      description:
+        "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
+    },
+    {
+      id: 4,
+      discount: 40,
+      cover: "./images/flash/flash-4.png",
+      name: "Smart Watch Black",
+      price: 50,
+      description:
+        "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
+    },
+    {
+      id: 5,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Shoes",
+      price: 100,
+      description:
+        "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
+    },
+    {
+      id: 6,
+      discount: 50,
+      cover: "./images/flash/flash-3.png",
+      name: "Shoes",
+      price: 100,
+      description:
+        "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
+    },
+  ],
 };
-  {*/
-    productItems: [
-      {
-        id: 1,
-        discount: 50,
-        cover: "./images/flash/flash-1.png",
-        name: "Shoes",
-        price: 100,
-        description:
-          "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
-      },
-      {
-        id: 2,
-        discount: 40,
-        cover: "./images/flash/flash-2.png",
-        name: "Watch",
-        price: 20,
-        description:
-          "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
-      },
-      {
-        id: 3,
-        discount: 40,
-        cover: "./images/flash/flash-3.png",
-        name: "Smart Mobile Black",
-        price: 200,
-        description:
-          "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
-      },
-      {
-        id: 4,
-        discount: 40,
-        cover: "./images/flash/flash-4.png",
-        name: "Smart Watch Black",
-        price: 50,
-        description:
-          "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
-      },
-      {
-        id: 5,
-        discount: 50,
-        cover: "./images/flash/flash-1.png",
-        name: "Shoes",
-        price: 100,
-        description:
-          "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
-      },
-      {
-        id: 6,
-        discount: 50,
-        cover: "./images/flash/flash-3.png",
-        name: "Shoes",
-        price: 100,
-        description:
-          "Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth.",
-      },
-    ],
-  }; 
 export default Data;
